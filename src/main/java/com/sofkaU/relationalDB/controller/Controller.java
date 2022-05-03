1bac84231bc2177dd32b0a767e07376457054a4e
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-03 16:35:41 -0500

    ADD: Creation of Controller

---
+++ src/main/java/com/sofkaU/relationalDB/controller/Controller.java
@@ -0,0 +1,47 @@
+package com.sofkaU.relationalDB.controller;
+
+import com.sofkaU.relationalDB.entities.Comment;
+import com.sofkaU.relationalDB.entities.Post;
+import com.sofkaU.relationalDB.service.CommentService;
+import com.sofkaU.relationalDB.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/")
+public class Controller {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping
+    public List<Post> getAllPosts(){
+        return postService.findAllPosts();
+    }
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post){
+        return postService.createPost(post);
+    }
+
+    @PostMapping
+    public Post createComment(@RequestBody Comment comment){
+        return commentService.createComment(comment);
+    }
+
+    @DeleteMapping
+    public void deletePost(@RequestBody Post post){
+        postService.deletePost(post);
+    }
+
+    @DeleteMapping
+    public void deleteComment(@RequestBody Comment comment){
+        commentService.deleteComment(comment);
+    }
+
+}
